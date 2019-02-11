bdfafcc46a41a6f60e26950b58ead8147adfb6b0
清水俊樹 <shimizutoshiki@MBA-3.local>
2019-02-11 19:44:00 +0900

    modify

--- tool/source/check_corr.py
+++ tool/source/check_corr.py
@@ -4,8 +4,7 @@ import pandas as pd
 import os
 import sys
 path = sys.argv[1]
-path = '/Users/shimizutoshiki/kaggle/mmp/output/'
-nrows = 100000 # Set in a sufficient number
+nrows = 10000 # Set in a sufficient number
 ls = os.listdir(path)
 dfs = [pd.read_csv(path+csv, nrows = nrows).iloc[:,-1] for csv in ls]
 df =  pd.concat(dfs, axis=1)
